930b4d714af0a414db6096baf119a2a55bef9bf3
Ben Farrell <ben@benfarrell.com>
2024-03-02 23:00:30 -0800

    fixed some timing issues by reducing precision and simplified some things

--- looprecord.rb
+++ looprecord.rb
@@ -1,12 +1,13 @@
 # when describing a recording track (setting which one)
 # the recording track is not 0 indexed like array access
-sequence = [   { notebuffer: [], playbackIndex: 0, startTime: 0, endTime: 0, numMeasures: 0, measureDuration: 0 }, #track 1
-               { notebuffer: [], playbackIndex: 0, startTime: 0, endTime: 0, numMeasures: 0, measureDuration: 0 }, #track 2
-               { notebuffer: [], playbackIndex: 0, startTime: 0, endTime: 0, numMeasures: 0, measureDuration: 0 }  #track 3
+sequence = [
+               { notebuffer: [], playbackIndex: 0, startTime: 0, endTime: 0 }, #track 2
+               { notebuffer: [], playbackIndex: 0, startTime: 0, endTime: 0 }, #track 2
+               { notebuffer: [], playbackIndex: 0, startTime: 0, endTime: 0 }  #track 3
 ];
 
 # current recording session holds times and which track is being recorded
-recordingSession = { 
+recordingSession = {
   sessionStart: -1,
   sessionEnd: -1,
   track: -1,
@@ -16,6 +17,7 @@ recordingSession = {
 
 set :bpm, 120
 set_link_bpm! 120
+measureDuration = 4
 
 
 live_loop :control_input do
@@ -33,11 +35,10 @@ live_loop :noteup_input do
     recordTrackIndex = recordingSession[:track] - 1
     sequence[recordTrackIndex][:notebuffer].reverse.each do |i|
       if noteoff == i[:note] && i[:uptime] == -1
-        i[:uptime] = vt - recordingSession[:sessionStart]
+        i[:uptime] = (vt - recordingSession[:sessionStart]).round(1)
         # keep upping the stop time until we stop recording
-        sequence[recordTrackIndex][:stopTime] = i[:uptime]
-        sequence[recordTrackIndex][:numMeasures] = (i[:uptime] / recordingSession[:measureDuration]).to_i
-        sequence[recordTrackIndex][:measureDuration] = recordingSession[:measureDuration]
+        recordingSession[:sessionEnd] = vt
+        sequence[recordTrackIndex][:endTime] = i[:downtime]
         break
       end
     end
@@ -58,16 +59,19 @@ live_loop :notedown_input do
   else
     use_synth :piano
     play note, amp: velocity/127.0
-    
+
     recordTrackIndex = recordingSession[:track] - 1
     if recordingSession[:track] != -1
       if sequence[recordTrackIndex][:notebuffer].length == 0
+        # first note down marks beginning of the recording session
+        # however the space between now and the start of the measure
+        # is taken into account when playing back
         recordingSession[:sessionStart] = recordingSession[:measureStart]
-        measureStartOffset = vt - recordingSession[:measureStart]
+        measureStartOffset = (vt - recordingSession[:measureStart]).round(1)
         sequence[recordTrackIndex][:startTime] = measureStartOffset
       end
-      notetime = vt - recordingSession[:sessionStart]
-      sequence[recordTrackIndex][:notebuffer].push({ 
+      notetime = (vt - recordingSession[:sessionStart]).round(1)
+      sequence[recordTrackIndex][:notebuffer].push({
        note: note,
        velocity: velocity,
        downtime: notetime,
@@ -96,11 +100,12 @@ live_loop :hihat do
   sleep 0.5
 end
 
-live_loop :track1 do trackplay 1 end
+#live_loop :track1 do trackplay 1 end
 live_loop :track2 do trackplay 2 end
-live_loop :heartbeat do heartbeat 4 end
+live_loop :heartbeat do heartbeat measureDuration end
 
 define :trackplay do |trackNum|
+  bpmScaledMeasureDuration = measureDuration * (60 / get(:bpm).to_f)
   trackIndex = trackNum - 1
   track = sequence[trackIndex]
   notes = track[:notebuffer]
@@ -109,27 +114,23 @@ define :trackplay do |trackNum|
     i = sequence[trackIndex][:playbackIndex]
     currNote = notes.ring[i]
     sequence[trackIndex][:playbackIndex] += 1
-    play currNote[:note], sustain: currNote[:uptime] - currNote[:downtime], amp: (currNote[:velocity]/127.0)
-    sleeptime = notes.ring[i+1][:downtime] - currNote[:downtime]
+    play currNote[:note], sustain: (currNote[:uptime] - currNote[:downtime]).round(1), amp: (currNote[:velocity]/127.0)
+    sleeptime = (notes.ring[i+1][:downtime] - currNote[:downtime]).round(1)
     if sleeptime < 0
       # looping around to beginning of buffer, get remaining time
       # in measure to sleep before resuming buffer again
-      puts track[:numMeasures], track[:measureDuration]
-      sleeptime = track[:numMeasures] * track[:measureDuration] - track[:endTime]
+      numMeasures = (track[:endTime] / bpmScaledMeasureDuration).to_i + 1
+      sleeptime = (bpmScaledMeasureDuration * numMeasures - track[:endTime] - track[:startTime]).round(1)
     end
-    sleep sleeptime * (get(:bpm) / 60)
+    sleep (sleeptime * (get(:bpm) / 60))
   else
-    sleep 4
+    sleep bpmScaledMeasureDuration
   end
 end
 
 define :heartbeat do |beatsPerMeasure|
   use_real_time
   recordingSession[:measureStart] = vt
-  #if measureDuration != -1
-    # realtimeOffset = vt % measureDuration (do we need this?)
-  #end
   sleep beatsPerMeasure
   recordingSession[:measureDuration] = vt - recordingSession[:measureStart]
 end
-
